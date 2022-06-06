4d68a0f05bb848dfd6edbe4cab09a60687ce2c81
Maurice Korish <mauricekorish@gmail.com>
2022-06-06 06:37:26 +0300

    Jinja Template Beta

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+
+@app.route('/aboutus')
+def aboutus():
+    return render_template('templatetest.html', nameOfPage="About Us", type="About Us", information="Luis - , Lucas - , Maurice - ",  url=os.getenv("URL"))
+
+@app.route('/hobbies')
+def hobbies():
+    return render_template('templatetest.html', nameOfPage="Hobbies", type="Our Hobbies", information="Luis - , Lucas - , Maurice - ",  url=os.getenv("URL"))
