0a7a42780dcd205067113addeb32a92d385d7e54
Luis Moraguez <lmoraguez6380@floridapoly.edu>
2022-06-30 05:12:02 -0400

    Added timeline CSS and placeholder

--- app/__init__.py
+++ app/__init__.py
@@ -87,7 +87,7 @@ def portfolioDetails():
 @app.route('/timeline')
 def timeline():
     data = get_time_line_post()
-
+    data = data[timeline_posts]
     print(data)
 
     return render_template('timeline.html', title="Timeline", data=data)
