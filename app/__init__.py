df6712089031f0690fe899996b0fd986c276ddae
Luis Moraguez <lmoraguez6380@floridapoly.edu>
2022-07-09 03:08:40 -0400

    Added delete button on timeline cards

--- app/__init__.py
+++ app/__init__.py
@@ -145,6 +145,6 @@ def delete_time_line_by_post(id):
     deleted = TimeLinePost.get(TimeLinePost.id == id)
     try:
         TimeLinePost.delete_by_id(id)
-        return redirect('/timeline')
+        return redirect(url_for('delete_time_line_by_post'))
     except:
         return "There was a problem trying to delete -> " + model_to_dict(deleted)
